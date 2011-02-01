9850ba506cc575e333e4017e078914800ff5d352
aloksrivastava78 <alok@chipmonk.com>
2011-02-02 00:11:04 +0530

    basic changes done

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
+#!/home/alok/.rvm/rubies/ruby-1.9.2-p136/bin/ruby
 require 'spec_helper'
 
 describe PagesController do
+render_views
+  
+  before(:each) do
+    @base_title = 'Clipper'
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it 'should pass title test' do
+      get 'home'
+      response.should have_selector('title', :content => "#{@base_title} Home")
     end
   end
 
@@ -14,6 +24,36 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it 'should pass title test' do
+      get 'contact'
+      response.should have_selector('title', :content => "#{@base_title} Contact")
+    end
+
   end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it 'should pass title test' do
+      get 'about'
+      response.should have_selector('title', :content => "#{@base_title} About")
+    end
+
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it 'should pass title test' do
+      get 'help'
+      response.should have_selector('title', :content => "#{@base_title} Help")
+    end
+
+  end
+
 
 end
