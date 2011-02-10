8bb21482ceb84ddcd7c2531b04bd003dcc1c5f7f
Alok Srivastava Ofiice <aloksrivastava78@gmail.com>
2011-02-11 04:14:45 +0530

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
   def new
     @title = 'Sign up'
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #handle success
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
   def show
