c26f6dc4de7aa232a0eeb60027dd411a046cbcd0
Alok Srivastava Ofiice <aloksrivastava78@gmail.com>
2011-02-15 21:27:21 +0530

    updated

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def sign_in(user)
+    cookies.signed.permanent[:remeber_user] = [user.id, user.salt]
+    self.current_user = user
+  end
+  def current_usee = user
+    @current_user = user
+  end
 end
